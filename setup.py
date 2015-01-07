68fd09c9b306d683d61cdb62b10fc9a57fc44bc5
Chris Villa <chrisvilla@me.com>
2015-01-07 22:50:27 +0000

    bumped to 0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='micron',
-    version='0.0.1',
+    version='0.0.2',
     author='Chris Villa',
     author_email='chrisvilla@me.com',
     url='https://github.com/chrisvxd/micron',
